581bcd0e053c9256afca79f5cfa84cc3b55b81c0
Krzysztof <Krzysztof@appformation.pl>
2015-11-27 14:14:11 +0100

    Update: 8

--- _8_game_with_leds/_8_game_with_leds.ino
+++ _8_game_with_leds/_8_game_with_leds.ino
@@ -2,17 +2,18 @@
 
 int leds[4] = {9,10,11,12};
 int light;
-int action_x = 5;
-int action_y = 6;
+int action_x = A2;
+int action_y = A3;
 int value_x;
 int value_y;
+int value;
 
 int total_score = 0;
 int points = 0;
 int base_points = 100;
 
 int start_time = 0;
-int base_game_time = 10;
+int base_game_time = 15;
 
 String text = "";
 
@@ -32,25 +33,18 @@ void random_led(){
 }
 
 int catch_led(){
-  // TODO
-  /* This part is used to test buttons. Remove it if you use a joystick */
-  int lx[2] = {1,2};
-  int ly[2] = {0,3};
-  if (value_x==1 && value_y==0)  { return lx[random(0,2)]; }
-  if (value_x==0 && value_y==1)  { return ly[random(0,2)]; }
-  /* ========================================================= */
-  
-  if (value_x==1 && value_y==0)  { return 1; } //right
-  if (value_x==-1 && value_y==0) { return 2; } //left
-  if (value_x==0 && value_y==1)  { return 3; } //up
-  if (value_x==0 && value_y==-1) { return 0; } //down
+  if (value_x==-1 && value_y==0) { return 1; } //right
+  if (value_x==1  && value_y==0) { return 2; } //left
+  if (value_x==0  && value_y==1) { return 3; } //up
+  if (value_x==0  && value_y==-1){ return 0; } //down
   return -1;
 }
 
 int get_action(int pin){
-  // TODO
-  /* use this function to return joystick's input mapped to {-1,0,1} */
-  return digitalRead(pin);
+  value = analogRead(pin);
+  if (value < 250) { return -1; }
+  if (value > 750) { return 1;  }
+  return 0;
 }
 
 void game(){
@@ -65,11 +59,15 @@ void game(){
     
     if (light == catch_led()) {
       digitalWrite(leds[light], LOW);
-      Serial.println(points);
+      //Serial.println(points);
       total_score += points;
       points = base_points;
-      
-      delay(500);
+      delay(300);
+      do {
+        value_x = get_action(action_x);
+        value_y = get_action(action_y);
+      }
+      while (!(value_x==0 && value_y==0)); 
       random_led();
     }
     if (points > 0) { points--; }
